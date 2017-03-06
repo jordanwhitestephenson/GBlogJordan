22fe591708c7c4291b60e22690d3d4755f685f31
Jordan Stephenson <jordanwhitestephenson@gmail.com>
2017-03-06 15:11:35 -0700

    Initial Heroku

--- seeds/comments.js
+++ seeds/comments.js
@@ -7,7 +7,15 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('comment').insert([
-        {body: 'This Blog IS AWESOME! COMMENTS'},
+        {body: 'This Blog IS AWESOME! COMMENTS',
+        username_id: knex('username').where('email', 'jordanwhitestephenson@gmail.com').select('id'),
+         blog_id: knex('blog').where('id', 1).select('id')},
+         {body: 'This blog sucks, horrible post',
+         username_id: knex('username').where('email', 'jordanwhitestephenson@gmail.com').select('id'),
+          blog_id: knex('blog').where('id', 2).select('id')},
+          {body: 'I would say this post is a 7 out of 10',
+          username_id: knex('username').where('email', 'jordanwhitestephenson@gmail.com').select('id'),
+           blog_id: knex('blog').where('id', 2).select('id')}
       ]);
     });
 };
